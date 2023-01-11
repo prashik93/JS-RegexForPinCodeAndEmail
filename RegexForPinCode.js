c9f19da1bea1dac0fa4ed1a0394e4e3cc32c14ee
Prashik Kamble <kambleprashik7349@gmail.com>
2023-01-11 20:10:10 +0530

    ADDED : Code To Create Regex Pattern To Validate The Pin Code

---
+++ RegexForPinCode.js
@@ -0,0 +1,29 @@
+class RegexForPinCode {
+
+    constructor(...params) {
+        this.pinCode = params[0];
+    }
+
+    get pinCode() {return this._pinCode;}
+    set pinCode(pinCode) {
+        let pinCodeRegex = RegExp('[1-9][0-9]{5}');
+        if(pinCodeRegex.test(pinCode)){
+            this._pinCode = pinCode;
+        }
+        else throw "PinCode is Incorrect!"
+    }
+
+    toString() {
+        return "pinCodeRegex=" + this.pinCode;
+    }
+}
+
+let regexForPinCode = new RegexForPinCode("400088");
+console.log(regexForPinCode.toString());
+
+try{
+    regexForPinCode.pinCode = "444";
+    console.log(regexForPinCode.toString());
+} catch(e) {
+    console.error(e);
+}
