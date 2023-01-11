8d269ee478fdbae6c0464d87bfc445b6807e2753
Prashik Kamble <kambleprashik7349@gmail.com>
2023-01-11 20:31:36 +0530

    MODIFIED : Code To Validate The Pin Code i.e 400 088 Is Also Valid Along With 400088

--- RegexForPinCode.js
+++ RegexForPinCode.js
@@ -6,7 +6,7 @@ class RegexForPinCode {
 
     get pinCode() {return this._pinCode;}
     set pinCode(pinCode) {
-        let pinCodeRegex = RegExp('^[1-9][0-9]{5}$');
+        let pinCodeRegex = RegExp('(^[1-9][0-9]{5}|(^[1-9][0-9]{2}[\\s][0-9]{3}))$');
         if(pinCodeRegex.test(pinCode)){
             this._pinCode = pinCode;
         }
@@ -22,7 +22,7 @@ let regexForPinCode = new RegexForPinCode("400088");
 console.log(regexForPinCode.toString());
 
 try{
-    regexForPinCode.pinCode = "400088B";
+    regexForPinCode.pinCode = "400 088";
     console.log(regexForPinCode.toString());
 } catch(e) {
     console.error(e);
