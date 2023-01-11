f9c4d4f74a0369c46590624eb4a78ece3dedebbb
Prashik Kamble <kambleprashik7349@gmail.com>
2023-01-11 21:19:21 +0530

    MODIFIED : Code To Ensure @ And Validate The Mandatory Second Part i.e bridgelabz

--- RegexForEmail.js
+++ RegexForEmail.js
@@ -6,7 +6,7 @@ class RegexForEmail {
 
     get email() {return this._email;}
     set email(email) {
-        let emailRegex = RegExp('^[a-z]{2,}');
+        let emailRegex = RegExp('^[a-z]{3}[@][a-z]{3,}$');
         if(emailRegex.test(email)){
             this._email = email;
         }
@@ -18,11 +18,11 @@ class RegexForEmail {
     }
 }
 
-let regexForEmail = new RegexForEmail("abc");
+let regexForEmail = new RegexForEmail("abc@bridgelabz");
 console.log(regexForEmail.toString());
 
 try{
-    regexForEmail.email = "a";
+    regexForEmail.email = "abcbridgelabz";
     console.log(regexForEmail.toString());
 } catch(e) {
     console.error(e);
