4ace007f544389e4858ce08bb65e2031f81c042b
Prashik Kamble <kambleprashik7349@gmail.com>
2023-01-11 22:14:11 +0530

    MODIFIED : Code To Clear All Email Samples

--- RegexForEmail.js
+++ RegexForEmail.js
@@ -6,11 +6,13 @@ class RegexForEmail {
 
     get email() {return this._email;}
     set email(email) {
-        let emailRegex = RegExp('^[a-z]{3}[.|_|+|-][a-z]{3,}[@][a-z]{3,}[.][co]{2}[.][a-z]{2}$');
+        let emailRegex = RegExp('^([a-z 0-9]{3,}|[a-z 0-9]{3,}[-|+|-|.][a-z 0-9]{1,})[@][a-z 0-9]{1,}[.]([com|net|co]{2,3}|[com|net|co]{2,3}[.][au|in|com]{2,3})$');
         if(emailRegex.test(email)){
             this._email = email;
+            validEmailsArray.push(email);
+        } else {
+            inValidEmailsArray.push(email);
         }
-        else throw "Email is Incorrect!"
     }
 
     toString() {
@@ -18,12 +20,28 @@ class RegexForEmail {
     }
 }
 
-let regexForEmail = new RegexForEmail("abc-xyz@bridgelabz.co.in");
-console.log(regexForEmail.toString());
+let allEmailsArray = ["abc@yahoo.com", "abc-100@yahoo.com", "abc.100@yahoo.com", "abc111@abc.com",
+"abc-100@abc.net", "abc.100@abc.com.au", "abc@1.com", "abc@gmail.com.com",
+"abc+100@gmail.com", "abc", "abc@.com.my", "abc123@gmail.a", "abc123@.com",
+"abc123@.com.com", ".abc@abc.com", "abc()*@gmail.com", "abc@%*.com",
+"abc..2002@gmail.com", "abc.@gmail.com", "abc@abc@gmail.com", "abc@gmail.com.1a",
+"abc@gmail.com.aa.au"];
 
-try{
-    regexForEmail.email = "abc@bridgelabz.co";
-    console.log(regexForEmail.toString());
-} catch(e) {
-    console.error(e);
+let validEmailsArray = new Array();
+let inValidEmailsArray = new Array();
+let emailRegex = RegExp('^[a-z]{3}[.|_|+|-][a-z]{3,}[@][a-z]{3,}[.][co]{2}[.][a-z]{2}$');
+let regexForEmail = new RegexForEmail();
+
+for(let i = 0; i < allEmailsArray.length; i++) {
+    regexForEmail.email = allEmailsArray[i];
+}
+
+console.log("\nValid Email Are : ");
+for(let i = 0; i < validEmailsArray.length; i++) {
+    console.log(validEmailsArray[i]);
+}
+
+console.log("\nInValid Email Are : ");
+for(let i = 0; i < inValidEmailsArray.length; i++) {
+    console.log(inValidEmailsArray[i]);
 }
